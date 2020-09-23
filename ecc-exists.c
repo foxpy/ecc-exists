7b1a1229f98d17ab759c3d93ce4aba0a3001172f
Murad <muradkarammaev@gmail.com>
2020-09-23 07:30:39 +0300

    add verbosity flag

--- ecc-exists.c
+++ ecc-exists.c
@@ -4,16 +4,19 @@
 
 int main(int argc, char* argv[]) {
 	size_t q, n, k, d;
+	bool verbose = false;
 	qc_args* args = qc_args_new();
 	qc_args_unsigned(args, "q", &q, "Code bitness");
     qc_args_unsigned(args, "n", &n, "Code block length");
     qc_args_unsigned(args, "k", &k, "Information bits");
     qc_args_unsigned(args, "d", &d, "Hamming distance");
+    qc_args_flag(args, 'v', "verbose", &verbose, "Spam terminal with thoughts");
     char* err;
     if (qc_args_parse(args, argc, argv, &err) == -1) {
         fprintf(stderr, "Failed to parse command line arguments: %s\n", err);
         free(err);
         exit(EXIT_FAILURE);
     }
-    printf("Going to analyze if following ECC exists: q=%zu n=%zu k=%zu d=%zu...\n", q, n, k, d);
+    printf("Going to analyze if following ECC exists: q=%zu n=%zu k=%zu d=%zu\n", q, n, k, d);
+    printf("while %s verbose...\n", verbose ? "being" : "not being");
 }
